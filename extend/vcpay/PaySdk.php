4b47f6fe71310f9f60326ef95ea2026ae51bfe8a
waheilee <waheilee@gmail.com>
2023-10-25 21:58:29 +0800

    feat：mkcpay

--- extend/vcpay/PaySdk.php
+++ extend/vcpay/PaySdk.php
@@ -5,6 +5,7 @@
  * Date: 2021/6/7
  * Time: 14:55
  */
+
 //墨西哥
 namespace vcpay;
 
@@ -36,7 +37,7 @@ class PaySdk
 
     public function pay($param, $config = [])
     {
-        
+
         if (isset($config['merchant']) && !empty($config['merchant'])) {
             $this->merchant = $config['merchant'];
         }
@@ -49,42 +50,42 @@ class PaySdk
         if (isset($config['apiurl']) && !empty($config['apiurl'])) {
             $this->api_url = $config['apiurl'];
         }
-        $merchant    = $this->merchant;
-        $appid       = $this->appid;
-        $orderid     = trim($param['orderid']);
-        $amount      = sprintf('%.2f',$param['amount']);
-        $notify_url  = trim($config['notify_url']);
-        $mobile      = rand(6,9).rand(100000000,999999999);
-        $username    = chr(rand(65,90)).chr(rand(97,122)).chr(rand(97,122)).chr(rand(97,122)).chr(rand(97,122)).chr(rand(97,122));
-        $email       = $mobile.'@gmail.com';
-        $timestamp   = time();
-        
-        $amount = (int)$amount*100;
+        $merchant = $this->merchant;
+        $appid = $this->appid;
+        $orderid = trim($param['orderid']);
+        $amount = sprintf('%.2f', $param['amount']);
+        $notify_url = trim($config['notify_url']);
+        $mobile = rand(6, 9) . rand(100000000, 999999999);
+        $username = chr(rand(65, 90)) . chr(rand(97, 122)) . chr(rand(97, 122)) . chr(rand(97, 122)) . chr(rand(97, 122)) . chr(rand(97, 122));
+        $email = $mobile . '@gmail.com';
+        $timestamp = time();
+
+        $amount = (int)$amount * 100;
         $data = [
-            'app_id'=>$appid,
-            'nonce_str'=>rand(10000000,99999999).sprintf('%.0f', floatval(explode(' ', microtime())[0]) * 1000),
-            'trade_type'=>$config['code'],
-            'order_amount'=>$amount,
-            'out_trade_no'=>$orderid,
-            'notify_url'=>$notify_url,
-            'back_url'=>$config['redirect_url']
+            'app_id' => $appid,
+            'nonce_str' => rand(10000000, 99999999) . sprintf('%.0f', floatval(explode(' ', microtime())[0]) * 1000),
+            'trade_type' => $config['code'],
+            'order_amount' => $amount,
+            'out_trade_no' => $orderid,
+            'notify_url' => $notify_url,
+            'back_url' => $config['redirect_url']
         ];
 
         $header = [
             'Content-Type:application/json;charset=UTF-8',
         ];
-        $data['sign'] = $this->createSign($data,$this->secret);
+        $data['sign'] = $this->createSign($data, $this->secret);
 
-        $result = $this->curl_post_content($this->api_url.'/pay/save',json_encode($data), $header);
+        $result = $this->curl_post_content($this->api_url . '/pay/save', json_encode($data), $header);
 
-        save_log('vcpay','提交参数:'.json_encode($data).',接口返回信息：'.$result.'-----请求路径：'.$this->api_url);
+        save_log('vcpay', '提交参数:' . json_encode($data) . ',接口返回信息：' . $result . '-----请求路径：' . $this->api_url);
         $res = json_decode($result, true);
         if (!isset($res['code'])) {
-            $res['message'] ='Http Request Invalid';
+            $res['message'] = 'Http Request Invalid';
         }
-        $returl='';
-        if(is_array($res)){
-            if($res['code']=='200'){
+        $returl = '';
+        if (is_array($res)) {
+            if ($res['code'] == '200') {
                 $returl = $res['pay_url'];
             }
         }
@@ -93,19 +94,24 @@ class PaySdk
     }
 
     //回调地址 /client/Pay_Notify/templatepay_notify
-    public function notify($params,$header=[],$channel,$logname)
+    public function notify($params, $header = [], $channel, $logname)
     {
         try {
             //参数
-            $sign                  = $params['sign']??'';
-            $data['json']          = json_encode($params);
-            $data['orderid']       = $params['out_trade_no']??'';   //平台内部订单号
-            $data['realmoney']     = $params['order_amount']??'';
-            $data['transactionId'] = $params['trade_no']??'';    //三方订单号
-            $data['code']          = $params['trade_state'];
-            $data['status']        = $params['trade_state']??'' == '1' ? 1 : 0;
+            $sign = $params['sign'] ?? '';
+            $data['json'] = json_encode($params);
+            $data['orderid'] = $params['out_trade_no'] ?? '';   //平台内部订单号
+            $data['realmoney'] = $params['order_amount'] ?? '';
+            $data['transactionId'] = $params['trade_no'] ?? '';    //三方订单号
+            $data['code'] = $params['trade_state'];
+            if ($params['trade_state'] == 0 && $params['code'] == '200' && $params['msg'] == 'SUCCESS') {
+                $data['status'] = 1;
+            } else {
+                $data['status'] = $params['trade_state'] ?? '' == '1' ? 1 : 0;
 
-            $data['realmoney']     = $data['realmoney']/100;
+            }
+
+            $data['realmoney'] = $data['realmoney'] / 100;
             //sign认证
             unset($params['sign']);
             unset($params['code']);
@@ -117,12 +123,12 @@ class PaySdk
             if (empty($channel_all)) {
                 exit('fail:Channel Not Exist');
             }
-            $channel = json_decode($channel_all['MerchantDetail'],true);
+            $channel = json_decode($channel_all['MerchantDetail'], true);
             $channel['ChannelId'] = $channel_all['ChannelId'];
             // }
 
-            $checksign             = $this->createSign($params,$channel['secret']);
-            (new \paynotify\PayNotify('success'))->notify($data,$sign,$checksign,$channel,$logname);
+            $checksign = $this->createSign($params, $channel['secret']);
+            (new \paynotify\PayNotify('success'))->notify($data, $sign, $checksign, $channel, $logname);
         } catch (Exception $ex) {
             save_log($logname, 'Exception:' . $ex->getMessage() . $ex->getLine() . $ex->getTraceAsString());
             exit('fail');
@@ -130,16 +136,17 @@ class PaySdk
     }
 
     //回调地址 /client/Pay_Notify/templatepay_outnotify
-    public function outnotify($params,$header=[],$channel,$logname){
+    public function outnotify($params, $header = [], $channel, $logname)
+    {
         try {
             //参数
-            $sign                  = $params['sign']??'';
-            $data['json']          = json_encode($params);
-            $data['orderid']       = $params['out_trade_no']??'';
-            $data['realmoney']     = $params['order_amount']??'';
-            $data['transactionId'] = $params['trade_no']??'';
-            $data['code']          = $params['trade_state'];
-            $data['realmoney']     = $data['realmoney']/100;
+            $sign = $params['sign'] ?? '';
+            $data['json'] = json_encode($params);
+            $data['orderid'] = $params['out_trade_no'] ?? '';
+            $data['realmoney'] = $params['order_amount'] ?? '';
+            $data['transactionId'] = $params['trade_no'] ?? '';
+            $data['code'] = $params['trade_state'];
+            $data['realmoney'] = $data['realmoney'] / 100;
             switch ($data['code']) {
                 case '1':
                     //成功
@@ -170,17 +177,17 @@ class PaySdk
 //            $channel = json_decode($channel_all['MerchantDetail'],true);
 //            $channel['ChannelId'] = $channel_all['ChannelId'];
 
-            $checksign             = $this->createSign($params,$channel['secret']);
+            $checksign = $this->createSign($params, $channel['secret']);
 
-            (new \paynotify\PayNotify('success'))->outnotify($data,$sign,$checksign,$channel,$logname);
-        }catch (Exception $ex){
-            save_log($logname,'Exception:' . $ex->getMessage().$ex->getLine().$ex->getTraceAsString());
+            (new \paynotify\PayNotify('success'))->outnotify($data, $sign, $checksign, $channel, $logname);
+        } catch (Exception $ex) {
+            save_log($logname, 'Exception:' . $ex->getMessage() . $ex->getLine() . $ex->getTraceAsString());
             exit('fail');
         }
     }
 
 
-    private function createSign($data,$Md5key)
+    private function createSign($data, $Md5key)
     {
         ksort($data);
         $md5str = '';
@@ -189,7 +196,7 @@ class PaySdk
                 $md5str = $md5str . $key . '=' . $val . '&';
             }
         }
-        $str =$md5str . 'key=' . $Md5key;
+        $str = $md5str . 'key=' . $Md5key;
         return strtoupper(md5($str));
     }
 
